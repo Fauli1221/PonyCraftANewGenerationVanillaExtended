f0fc6e3753dbac75d887d2de8d73c796110c6083
Fauli1221 <contact@fauli1221.de>
2024-03-29 11:41:25 +0100

    Revert "updated mc modpack kit"
    This reverts commit d93c163eee4d3c665071578c59e985f3afe84d0b.

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       systems = ["x86_64-linux"];
 
       imports = [
-        inputs.pre-commit-hooks.flakeModule
+        ./pre-commit-hooks.nix
       ];
 
       perSystem = {
@@ -24,21 +24,15 @@
         pkgs,
         ...
       }: {
-        pre-commit = {
-          check.enable = true;
-          settings.excludes = ["flake.lock"];
-          settings.hooks = {
-            alejandra.enable = true;
-            deadnix.enable = true;
-            prettier.enable = true;
-            markdownlint.enable = true;
-          };
-        };
         devShells.default = pkgs.mkShell {
           packages = [
+            pkgs.alejandra
             pkgs.git
             pkgs.packwiz
+            pkgs.yamllint
           ];
+          name = "mc-modpack-kit";
+          DIRENV_LOG_FORMAT = "";
           shellHook = ''
             ${config.pre-commit.installationScript}
           '';
